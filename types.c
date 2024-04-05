36c06351811a76d041a4ad73c503bbb864122e92
Richard Hansen <rhansen@rhansen.org>
2024-04-05 16:10:33 -0400

    Resolve the obsolescence warnings added by `autoupdate`

--- types.c
+++ types.c
@@ -4,16 +4,10 @@
 
 #include "pgpdump.h"
 
-#if TIME_WITH_SYS_TIME
+#if HAVE_SYS_TIME_H
 # include <sys/time.h>
-# include <time.h>
-#else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
 #endif
+#include <time.h>
 
 #if HAVE_STRUCT_TM_TM_ZONE
 # define tm_zone(tm) (tm->tm_zone)
